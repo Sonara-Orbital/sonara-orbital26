03e91b946fdd41c3255548ce85405fcdd6935caa
AaronL248 <aaronleongch@gmail.com>
2026-05-26 18:39:32 +0800

    Added buttons and page routing for login, create account and forgot password

---
+++ app/signup/page.tsx
@@ -0,0 +1,77 @@
+"use client";
+import { useState } from "react";
+import Link from "next/link";
+
+export default function SignupPage() {
+    const handleSignup = async (e: React.FormEvent) => {
+        e.preventDefault();
+        // Send request to backend here
+        console.log({name})
+        console.log({email})
+        console.log({password})
+    }
+
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+    // send userData to backend
+    const userData = {
+        userName: name, 
+        emailAddress: email,
+        password: password 
+    }
+
+    return <div>
+        <div className="flex flex-col">
+            <h1 className="text-2xl font-bold mb-2">Create an Account</h1>
+            <form onSubmit={handleSignup} className="flex flex-col">
+                {/* Username */}
+                <div>
+                    <label className=""> 
+                        Username
+                    </label>
+                    <input type="text" placeholder="Jeff" value={name} 
+                        onChange={(e) => setName(e.target.value)}
+                        className="border border-2 border-neutral-500 my-1 ml-2"
+                        required/>
+                </div>
+
+                {/* Email */}
+                <div>
+                    <label className=""> 
+                        Email
+                    </label>
+                    <input type="text" 
+                        placeholder="ABC@gmail.com" 
+                        value={email} 
+                        onChange={(e) => setEmail(e.target.value)}
+                        className="border border-2 border-neutral-500 my-1 ml-2"
+                        required/>
+                </div>
+
+                {/* Password */}
+                <div>
+                    <label className=""> 
+                        Password
+                    </label>
+                    <input type="text" 
+                        placeholder="***" 
+                        value={password} 
+                        onChange={(e) => setPassword(e.target.value)}
+                        className="border border-2 border-neutral-500 my-1 ml-2"
+                        required/>
+                </div>
+                <button 
+                    type="submit"
+                    className="w-46 bg-neutral-500 text-gray-200">
+                        Create Account
+                    </button>
+            </form>
+
+            <p> Already have account? 
+                <Link href="/login" className="underline text-blue-500">Sign in</Link>
+            </p>
+        </div>
+    </div>
+}
