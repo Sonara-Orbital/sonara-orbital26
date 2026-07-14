36fe27cf535ebf9148f4f9ca28487f7141481f88
AaronL248 <aaronleongch@gmail.com>
2026-07-14 10:40:23 +0800

    Added lastfm API key

--- fastapi/app/mood_recommender.py
+++ fastapi/app/mood_recommender.py
@@ -5,12 +5,12 @@ from pydantic import BaseModel
 from dotenv import load_dotenv
 import numpy as np
 from sklearn.neighbors import NearestNeighbors
-import json, ast, os, joblib, spotipy
+import json, ast, joblib, spotipy, requests
 import musicbrainzngs as mbn
 import zstandard as zstd
 from spotipy.oauth2 import SpotifyClientCredentials
 from fastapi import APIRouter
-from database import supabase 
+from database import supabase, LASTFM_API_KEY
 
 router = APIRouter()
 
@@ -33,3 +33,14 @@ async def get_mood_recommendations(mood_input: MoodInput):
     # 3. Fetch song titles from supabase
 
     return {"status": "success", "data": result}
+
+
+def test_tags():
+    url = f"https://ws.audioscrobbler.com/2.0/?method=track.gettoptags&api_key={LASTFM_API_KEY}&artist=one+direction&track=history&format=json"
+    response = requests.get(url)
+    data = response.json()
+    print(data)
+    for song in data['toptags']['tag']:
+        print(song['name'])
+
+test_tags()
