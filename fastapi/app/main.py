52143c75652b1d1dd0e36bb33716b5882607ed6b
AaronL248 <aaronleongch@gmail.com>
2026-07-24 02:14:37 +0800

    Changed CORS to include deployment

--- fastapi/app/main.py
+++ fastapi/app/main.py
@@ -20,7 +20,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000", "https://sonara-orbital26-git-deploy-test-sonara1.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
